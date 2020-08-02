2ae9ec21e9eda095f54eb6c4ca7bd10cf61c00f6
Matthew Wagaman <averaqedev@gmail.com>
2020-08-01 18:53:44 -0700

    added dependencies for shopify api call

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
+  SHOPIFY_ENDPOINT: process.env.SHOPIFY_ENDPOINT || 'change-this',
   SHOPIFY_API_KEY: process.env.SHOPIFY_API_KEY || 'change-this',
   SHOPIFY_PASSWORD: process.env.SHOPIFY_PASSWORD || 'change-this'
 };
--- src/products/products-service.js
+++ src/products/products-service.js
@@ -1,17 +1,24 @@
 const config = require('../config');
+const fetch = require('node-fetch');
 
 const ProductsService = {
   getAllProducts() {
     return fetch(config.SHOPIFY_ENDPOINT, {
       method: 'GET',
-      Authorization: `Basic ${this.getShopifyAuth()}`,
+      headers: {
+        Authorization: `Basic ${this.getShopifyAuth()}`
+      },
     })
       .then(resp => resp.json());
   },
 
   getShopifyAuth() {
     const authString = `${config.SHOPIFY_API_KEY}:${config.SHOPIFY_PASSWORD}`;
-    return btoa(authString);
+    return this.btoa(authString);
+  },
+
+  btoa(string) {
+    return Buffer.from(string).toString('base64');
   }
 };
 
